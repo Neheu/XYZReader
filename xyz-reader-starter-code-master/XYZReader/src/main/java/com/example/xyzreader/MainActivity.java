f54a467da183f2e8ed1cdc553b37b7e388c0d441
Neha <neha.sharma.unique@gmail.com>
2017-05-24 13:58:27 +0530

    Loading and UI issue in detail screen

---
+++ xyz-reader-starter-code-master/XYZReader/src/main/java/com/example/xyzreader/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.xyzreader;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.xyzreader.ui.ArticleListActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artical_new);
+    }
+
+
+}
